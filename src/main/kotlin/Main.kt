aebbc8dfd64ac0d457b12fcb1e66be07c088b78d
Sara S Camilleri <ssancam391@g.educaand.es>
2024-02-02 11:24:07 +0100

    implementacion_nuevaPersona

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,81 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+open class Persona(val nombre: String, edad: Int = 0) {
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+    var edad: Int = edad
+        private set(value) {
+            requireEdad()
+            field = value
         }
-    }
-}
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    init {
+        //requireEdad()
+        require(nombre.isNotBlank()) { "El nombre no puede ser una cadena vacía." }
     }
+
+    fun cumple() {
+        println("Ha cunmplido ${++edad} añitos!!")
+    }
+
+    open fun actividad(): String {
+        return ("$nombre está realizando una actividad")
+    }
+
+    private fun requireEdad() {
+        require(edad >= 0) { "La edad no puede ser negativa" }
+    }
+
+    override fun toString(): String {
+        return "Persona (nombre: $nombre, edad: $edad.)"
+    }
+
+
+}//FIN CLASS PERSONA
+
+class Estudiante(nombre: String, edad: Int, val carrera: String) : Persona(nombre, edad) {
+    override fun toString(): String {
+        return ("${super.toString().dropLast(2).replace("Persona (", "Estudiante (")}, carrera: $carrera)")
+    }
+
+    override fun actividad(): String {
+        return "$nombre está estudiando a ratos..."
+    }
+
+
+}//FIN CLASE ESTUDIANTE
+
+fun pedirPersona(): Persona {
+    var p: Persona? = null
+    do {
+        try {
+            println("Introduzca el nombre: ")
+            val nombre = readln()
+
+            println("Introduzca la edad: ")
+            val edad = readln().toInt()
+
+            p = Persona(nombre, edad)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        } catch (e: NumberFormatException) {
+            println("La edad debe ser un número entero positivo o 0.")
+        }
+    } while (p == null)
+    return p
+}
+fun main() {
+    val p = Persona("Juan", 55)
+    println(p)
+
+    p.cumple().also { println("Ha cumplido añitos") }
+    println(p)
+    //println("Nombre: $p.nombre, Edad: $p.edad")
+
+    val e = Estudiante("Pablo", 23, "Medicina")
+    println(e)
+    println(e.actividad())
+
+    e.cumple()
+    println(e)
+
+    val nuevaPersona = pedirPersona()
+    println (nuevaPersona)
 }
